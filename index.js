609c050ea704a672041b708db0e35aad116ee28d
Mark Poepping <mark.poepping@mmpro.de>
2018-11-23 16:13:16 +0100

    0.0.5 Allow numbers and signs at the beginning of the text

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const parser = (function () {
   pItems.fromSrt = function (data, ms) {
     const useMs = !!ms
     let regex = '(\\d+)\\n(\\d{1,2}:\\d{2}:\\d{2},\\d{3}) --> (\\d{1,2}:\\d{2}:\\d{2},\\d{3})\\n'
-        regex+= '((((\\d{0,}\\s{1}){1,}[\\pL\\pP\\pS].*\\n)|([\\pL\\pP\\pS].*\\n)){1,})'
+        regex+= '((((\\d{0,}\\s{1}){1,}[\\pL\\pP\\pS].*\\n)|([\\pN\\pL\\pP\\pS].*\\n)){1,})'
     const items = []
 
     data = data.replace(/\r/g, '')
